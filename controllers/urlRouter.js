f0e0185680c2f1fbf7781c8702538d617be90578
daczecha <daczecha@gmail.com>
2022-02-18 16:35:12 +0400

    backend fix errors

--- controllers/urlRouter.js
+++ controllers/urlRouter.js
@@ -3,17 +3,18 @@ const Url = require('../models/Url');
 
 const urlRouter = require('express').Router();
 
-urlRouter.get('/:code', (request, response) => {
+urlRouter.get('/:code', async (request, response) => {
   const code = request.params.code;
-  const result = Url.findOne({ code });
-  response.redirect(result.url);
+  const result = await Url.findOne({ code });
+  response.redirect(`http://${result.url}`);
 });
 
 urlRouter.post('/', async (request, response) => {
   const url = request.body;
   const code = randomstring.generate(7);
   const obj = { url, code };
-  await Url.insertOne(obj);
+  const newUrl = new Url(obj);
+  await newUrl.save();
   response.send(obj);
 });
 
